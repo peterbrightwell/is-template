03f29e636159e29925ccfc3c79f5f2f40520261e
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-10-25 12:36:35 +0100

    This should pass, with colour

--- .ci-scripts/lint.sh
+++ .ci-scripts/lint.sh
@@ -7,19 +7,23 @@ for i in APIs/*.raml; do
         echo $i ok
     else
         cat output
-        echo $i failed
+        echo -e "\033[31m$i failed\033[0m"
         failed=y
     fi
 done
 
 echo Linting JSON...
 for i in APIs/schemas/*.json examples/*.json; do
-  if ./node_modules/.bin/jsonlint $i > /dev/null; then
-    echo $i ok
-  else
-    echo $i failed
+    if ./node_modules/.bin/jsonlint $i > /dev/null; then
+        echo $i ok
+    else
+        echo -e "\033[31m$i failed\033[0m"
     failed=y
   fi
 done
 
-[ $failed ] && exit 1
+if [ "$failed" == "y" ]; then
+    exit 1
+else
+    exit 0
+fi
