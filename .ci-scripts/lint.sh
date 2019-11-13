798dba7404c18d1f65a766c1f4f039d93ef05393
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-11-13 16:36:29 +0000

    added markdown linting

--- .ci-scripts/lint.sh
+++ .ci-scripts/lint.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-echo Linting RAML...
-for i in APIs/*.raml; do
-    perl -pi.bak -e 's/!include//' $i
-    if ./node_modules/.bin/yamllint $i > output; then
-        echo $i ok
-    else
-        cat output
-        echo -e "\033[31m$i failed\033[0m"
+
+if [ -d docs ]; then
+    echo Linting docs...
+    if ! ./node_modules/.bin/remark docs/ --use remark-preset-lint-recommended; then
         failed=y
     fi
-    mv $i.bak $i
-done
+fi
 
-echo Linting JSON...
-for i in APIs/schemas/*.json examples/*.json; do
-    if ./node_modules/.bin/jsonlint $i > /dev/null; then
-        echo $i ok
-    else
-        echo -e "\033[31m$i failed\033[0m"
-    failed=y
-  fi
-done
+if [ -d APIs ]; then
+    echo Linting APIs...
+    for i in APIs/*.raml; do
+        perl -pi.bak -e 's/!include//' $i
+        if ./node_modules/.bin/yamllint $i > output; then
+            echo $i ok
+        else
+            cat output
+            echo -e "\033[31m$i failed\033[0m"
+            failed=y
+            rm output
+        fi
+        mv $i.bak $i
+    done
+fi
+
+if [ -d APIs/schemas ]; then
+    echo Linting schemas...
+    for i in APIs/schemas/*.json ; do
+        if ./node_modules/.bin/jsonlint $i > /dev/null; then
+            echo $i ok
+        else
+            echo -e "\033[31m$i failed\033[0m"
+        failed=y
+      fi
+    done
+fi
+
+if [ -d examples ]; then
+    echo Linting examples...
+    for i in APIs/schemas/*.json ; do
+        if ./node_modules/.bin/jsonlint $i > /dev/null; then
+            echo $i ok
+        else
+            echo -e "\033[31m$i failed\033[0m"
+        failed=y
+      fi
+    done
+fi
 
 if [ "$failed" == "y" ]; then
     exit 1
